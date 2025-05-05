93463806f4f6eebace60cebfbef338dbef36fbc2
chris Taylor <ctay456a@gmail.com>
2025-05-05 16:19:25 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserSignIn from './UserSignIn';
+import StudentSignIn from './StudentSignIn';
+
+function App() {
+  return (
+    <div>
+      <UserSignIn />
+      <hr />
+      <StudentSignIn />
+    </div>
+  );
+}
+
+export default App;
